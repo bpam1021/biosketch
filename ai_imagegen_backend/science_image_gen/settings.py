b4b3712be54cde826b3261b454b7aa40a18a1cda
bpam1021 <blackpam1021@gmail.com>
2025-08-26 16:46:20 +0800

    some minor changes

--- ai_imagegen_backend/science_image_gen/settings.py
+++ ai_imagegen_backend/science_image_gen/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'science_image_gen_db',
         'USER': 'postgres',
-        'PASSWORD': 'biosketch123!',
+        'PASSWORD': 'Biosketch123!',
         'HOST': 'localhost',
         'PORT': '5432',
     }
